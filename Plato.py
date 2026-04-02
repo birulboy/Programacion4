e6a268d8b155b4f5f7ea4ae9dc1e66a2d31cca21
birulboy <brahian.castro@utp.edu.co>
2026-04-02 15:01:38 -0500

    Clase Plato Creada

---
+++ Plato.py
@@ -0,0 +1,13 @@
+class Plato:
+    def __init__(self, nombre, precio_base, es_vegetariano=False):
+        self.nombre = nombre
+        self._precio_base = precio_base
+        self.es_vegetariano = es_vegetariano
+ 
+    def precio(self):
+        return self._precio_base
+ 
+    def descripcion_detallada(self):
+        tipo = "Vegetariano" if self.es_vegetariano else "Estándar"
+        return f"{self.nombre} ({tipo}) - ${self._precio_base:,.0f}"
+ 
